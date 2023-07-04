2e1605081441d30df1f0c9c6c5e1e788db3dab38
Selvakumar13 <44002853+Selvakumar13@users.noreply.github.com>
2023-07-04 16:40:01 +0530

    Add files via upload

--- pdf_filter/Filter.py
+++ pdf_filter/Filter.py
@@ -5,7 +5,7 @@ import io
 import pandas as pd
 
 url_demo = 'https://demo.seqrdoc.com/'
-url = '	https://demo.seqrdoc.com/bestiu/backend/pdf_file/'
+url = 'https://demo.seqrdoc.com/bestiu/backend/pdf_file/'
 keyword = 'AGRICULTURE'
 filtered_urls = []
 
@@ -21,29 +21,35 @@ for link in soup.find_all('a'):
 
 # Extract text from a remote PDF file and check if it contains the target word
 def extract_text_from_remote_pdf(url, target_word):
-    response = requests.get(url, stream=True)
-    pdf_file = io.BytesIO(response.content)
+    occurrences = []
     try:
-        reader = PyPDF2.PdfReader(pdf_file)
-        for page in reader.pages:
-            text = page.extract_text()
-            if target_word.lower() in text.lower():
-                return True
-    except PyPDF2.utils.PdfReadError:
-        pass
-    return False
+        response = requests.get(url, stream=True)
+        pdf_file = io.BytesIO(response.content)
+        try:
+            reader = PyPDF2.PdfReader(pdf_file)
+            for page_number, page in enumerate(reader.pages, start=1):
+                text = page.extract_text()
+                if target_word.lower() in text.lower():
+                    occurrences.append(page_number)
+        except PyPDF2.utils.PdfReadError:
+            pass
+    except requests.exceptions.RequestException:
+        print(f'Error connecting to {url}. Skipping...')
+    return occurrences
 
-# Check if PDF files from selected URLs contain a keyword
+# Check the count and page numbers of the target word in PDF files from selected URLs
 print('Filtered URLs:')
-for url in filtered_urls:
-    if extract_text_from_remote_pdf(url, keyword):
-        print(url)
+for url in filtered_urls[1:10]:
+    occurrences = extract_text_from_remote_pdf(url, keyword)
+    count = len(occurrences)
+    print(f'{url}: {count} occurrences')
+    print(f'Page numbers: {occurrences}')
 
 # Export filtered URLs to Excel
 filtered_urls_with_keyword = [url for url in filtered_urls if extract_text_from_remote_pdf(url, keyword)]
 if filtered_urls_with_keyword:
     df = pd.DataFrame({'Filtered URLs': filtered_urls_with_keyword})
-    excel_file = 'filtered_urls.xlsx'
+    excel_file = 'filtered_urls1.xlsx'
     df.to_excel(excel_file, index=False)
     print('Filtered URLs exported to filtered_urls.xlsx')
 else:
