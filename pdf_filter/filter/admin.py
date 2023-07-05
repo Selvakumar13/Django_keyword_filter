a0a7385b33a983189a9502c2a3b42a7861403b68
Selvakumar13 <44002853+Selvakumar13@users.noreply.github.com>
2023-07-05 16:55:36 +0530

    Add files via upload

--- pdf_filter/filter/admin.py
+++ pdf_filter/filter/admin.py
@@ -1,37 +1,39 @@
 from django.contrib import admin
-from filter.models import Urls
+from django.http import HttpResponse
+import csv
+from .models import Urls
 import requests
 from bs4 import BeautifulSoup
-import PyPDF2
+import fitz
 import io
+import time
 
-def get_urls(Url):
+
+def get_urls(url):
     """
     Fetches the URLs for PDF files from the given URL directory.
 
     Args:
-        Url (str): The URL of the directory containing PDF files.
+        url (str): The URL of the directory containing PDF files.
 
     Returns:
         list: A list of URLs for the PDF files found in the directory.
     """
     url_demo = 'https://demo.seqrdoc.com/'
-    urls_list = [ ]
-    reqs = requests.get(Url)
+    urls_list = []
+    reqs = requests.get(url)
     soup = BeautifulSoup(reqs.text, 'html.parser')
 
-    # Getting the list of available urls for pdf files in our given directory
-
+    # Getting the list of available URLs for PDF files in the given directory
     for link in soup.find_all('a'):
         href = link.get('href')
         if href and href.endswith('.pdf') and not href.endswith('/.pdf'):
             if not href.startswith('http'):
                 href = f'{url_demo}{href}'
             urls_list.append(href)
-            urls_list.append(href)
-
 
     return urls_list
+
 
 def extract_text_from_remote_pdf(url, target_word):
     """
@@ -42,53 +44,59 @@ def extract_text_from_remote_pdf(url, target_word):
         target_word (str): The word to search for in the PDF file.
 
     Returns:
-        bool: True if the target word is found, False otherwise.
+        Tuple[List[int], int, bool]: A tuple containing a list of page occurrences, count, and a boolean indicating if the target word is found.
     """
-    for s_url in get_urls(url):
-                response = requests.get(s_url, stream=True)
-                pdf_file = io.BytesIO(response.content)
-                try:
-                    reader = PyPDF2.PdfReader(pdf_file)
-                    for page in reader.pages:
-                        text = page.extract_text()
-                        if target_word.lower() in text.lower():
-                            return True
-                except PyPDF2.utils.PdfReadError:
-                    pass
+    occurrences = []
+    count = 0
 
-                return False
+    response = requests.get(url, stream=True)
+    pdf_file = io.BytesIO(response.content)
+    try:
+        doc = fitz.open(stream=pdf_file, filetype='pdf')
+        for page_number in range(doc.page_count):
+            page = doc.load_page(page_number)
+            text = page.get_text()
+            if target_word.lower() in text.lower():
+                occurrences.append(page_number + 1)  # Adding 1 to match PyPDF2 page numbering
+                count += text.lower().count(target_word.lower())
+        if occurrences:
+            return occurrences, count, True
+    except fitz.errors.PDFError:
+        pass
 
-def check_keyword(modeladmin, request, queryset):
-    """
-    Custom admin action to check if PDF files from selected URLs contain a keyword.
+    return occurrences, count, False
 
-    Args:
-        modeladmin (django.contrib.admin.ModelAdmin): The ModelAdmin instance.
-        request (django.core.handlers.wsgi.WSGIRequest): The current request.
-        queryset (django.db.models.query.QuerySet): The selected Urls objects.
 
-    Returns:
-        None
-    """
-    filtered_urls = []
+def export_to_csv(modeladmin, request, queryset):
+    start_time = time.time()  # Start time
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="filtered_urls.csv"'
+    writer = csv.writer(response)
+    writer.writerow(['URL', 'Keyword', 'Page Numbers', 'Count', 'Keyword Found'])
+
     for url_obj in queryset:
         target_word = url_obj.keyword
         url = url_obj.Url
-        if extract_text_from_remote_pdf(url, target_word):
-            url_obj.keyword_found=True
-            url_obj.save()
-            filtered_urls.append(url_obj.Url)
+        urls = get_urls(url)
+        c=0
+        for s_url in urls:
+            occurrences, count, found = extract_text_from_remote_pdf(s_url, target_word)
+            writer.writerow([s_url, target_word, occurrences, count, str(found)])
+            c+=1
+            print(s_url, target_word, occurrences, count,c)
 
-    if filtered_urls:
-        print('Filtered URLs:')
-        for url in filtered_urls:
-            print(url)
-    else:
-        print('No URLs with the keyword found.')
+    end_time = time.time()  # End time
+    runtime = end_time - start_time
 
-class Filtered_url_admin(admin.ModelAdmin):
-    list_display = ['id', 'Url','keyword_found']
+    print(f"Runtime: {runtime} seconds")
+    return response
+
+
+class UrlsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'Url', 'keyword_found']
     ordering = ['id']
-    actions = [check_keyword]
+    actions = [export_to_csv]
 
-admin.site.register(Urls, Filtered_url_admin)
+
+admin.site.register(Urls, UrlsAdmin)
